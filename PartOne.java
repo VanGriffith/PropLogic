3292c9c096aa3f61a480bd2537882a71f53ed830
VanGriffith <vc13grif@siena.edu>
2023-04-04 14:33:13 -0400

    start

---
+++ PartOne.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PartOne {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String sentence = sc.nextLine();
+        sentence = sentence.replaceAll("\\s", "");
+        System.out.println(sentence);
+        sc.close();
+    }
+
+    static String biconditional(String sentence) {
+        String retval = "";
+        int charIndex = 0;
+
+        while (charIndex < 0) {
+            
+        }
+
+        return retval;
+    }
+}
