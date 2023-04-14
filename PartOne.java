941d0c153a957b53f8280bd6372514617d5f032f
VanGriffith <vc13grif@siena.edu>
2023-04-14 18:17:08 -0400

    aaaaa

--- PartOne.java
+++ PartOne.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
 
 public class PartOne {
+
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         String sentence = sc.nextLine();
         sentence = sentence.replaceAll("\\s", "");
         System.out.println(sentence);
+
         sc.close();
     }
 
     static String biconditional(String sentence) {
-        String retval = "";
-        int charIndex = 0;
+        if (!sentence.contains("=")) return sentence;
+        
+        String left = "(" + sentence.substring(0, sentence.indexOf("=")) + ")";
+        String right = "(" + sentence.substring(sentence.indexOf("=") + 1) + ")";
 
-        while (charIndex < 0) {
-            
-        }
-
-        return retval;
+        sentence = "("+left+">"+right+")^("+right+">"+left+")";
+        
+        return sentence;
     }
 }
