6d23546c68083918bffc35687ee51ca2175875c3
VanGriffith <vc13grif@siena.edu>
2023-04-16 18:21:54 -0400

    "wooop"

--- PartOne.java
+++ PartOne.java
@@ -10,15 +10,52 @@ public class PartOne {
 
         sc.close();
     }
+}
 
-    static String biconditional(String sentence) {
-        if (!sentence.contains("=")) return sentence;
-        
-        String left = "(" + sentence.substring(0, sentence.indexOf("=")) + ")";
-        String right = "(" + sentence.substring(sentence.indexOf("=") + 1) + ")";
+class Clause {
+    static final char NOT = '~';
+    static final char OPEN = '(';
+    static final char CLOSE = ')';
+    static final char AND = '&';
+    static final char OR = '|';
+    static final char IMPLIES = '>';
+    static final char EQUALS = '=';
+    static final char[] OPERATORS = {AND, OR, IMPLIES, EQUALS};
 
-        sentence = "("+left+">"+right+")^("+right+">"+left+")";
+    String string;
+    char operator;
+    int operatorIndex;
+    Clause a;
+    Clause b;
+
+    public Clause(String string) {
+        this.string = string;
+        String tempString = string.substring(string.indexOf(OPEN) + 1, string.length() - 1);
         
-        return sentence;
+        int parenCount = 0;
+        int index = 0;
+        boolean operatorFound = false;
+
+        while (!operatorFound) {
+            char currentChar = tempString.charAt(index);
+
+            if (currentChar == OPEN)  {
+                parenCount++;
+            }
+            else if (currentChar == CLOSE) {
+                parenCount--;
+            }
+            else if (parenCount == 0) {
+                for (char operator: OPERATORS) {
+                    if (currentChar == operator) {
+                        this.operator = currentChar;
+                        operatorFound = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        
     }
 }
